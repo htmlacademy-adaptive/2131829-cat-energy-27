024fcd06172ac353b9833520bb0bbe191a4008f6
MyCatAteAReptile <114024484+MyCatAteAReptile@users.noreply.github.com>
2022-12-27 22:49:05 +0500

    Homework 5: first review fixes

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,9 +1,7 @@
-let navMain = document.querySelector('.main-navigation');
-let navToggle = document.querySelector('.main-navigation__toggle');
-
+let navMain=document.querySelector('.main-navigation');
+let navToggle=document.querySelector('.main-navigation__toggle');
 navMain.classList.remove('main-navigation--nojs');
 navMain.classList.add('main-navigation--closed');
-
 navToggle.addEventListener('click', function () {
-  navMain.classList.toggle('main-navigation--closed');
+navMain.classList.toggle('main-navigation--closed');
 });
